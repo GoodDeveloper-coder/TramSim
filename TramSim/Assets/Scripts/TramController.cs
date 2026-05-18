118e988134e4960b43dc0fc261e6af589f11d629
GoodDeveloper-coder <itmancik@gmail.com>
2026-05-18 22:50:53 +0300

    Added tram follow rails

--- TramSim/Assets/Scripts/TramController.cs
+++ TramSim/Assets/Scripts/TramController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,24 +7,56 @@ public class TramController : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _maxSpeed = 100f;
+    [SerializeField] private float _minRotateRailDistance = 0.5f;
+    [SerializeField] private List<Transform> _railPoints = new List<Transform>();
 
     [Header("Inputs")]
     [SerializeField] private InputActionReference _moveInput;
-    private Rigidbody _rb;
+    private int _nextRailPoint = 0;
+    private float _currentSpeed = 0f;
+    private Vector3 _moveDirection;
+    private Vector3 _eulerAngles;
+    private float _followRotation = 0f;
 
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();
+        _moveDirection = transform.forward;
     }
 
     void Update()
     {
         Move();
+        RotateRail();
     }
 
     void Move()
     {
         float inputY = _moveInput.action.ReadValue<Vector2>().y;
-        _rb.AddForce(transform.forward * inputY * _speed, ForceMode.Force);
+        if (inputY != 0f)
+        {
+            _currentSpeed += _speed * inputY * Time.deltaTime;
+        }
+        else
+        {
+            _currentSpeed -= _speed * Time.deltaTime / 3;
+        }
+
+        _currentSpeed = Math.Clamp(_currentSpeed, 0, _maxSpeed);
+        transform.position += _moveDirection * _currentSpeed * Time.deltaTime;
+    }
+
+    void RotateRail()
+    {
+        if (Vector3.Distance(transform.position, _railPoints[_nextRailPoint].position) <= _minRotateRailDistance)
+        {
+            _moveDirection = _railPoints[_nextRailPoint++].forward;
+            _followRotation -= 45f;
+
+            if (_nextRailPoint >= _railPoints.Count)
+                _nextRailPoint = 0;
+        }
+        _eulerAngles = Vector3.Lerp(_eulerAngles, new Vector3(0, _followRotation, 0), 45 * Time.deltaTime); 
+        transform.localEulerAngles = _eulerAngles;
     }
 }
