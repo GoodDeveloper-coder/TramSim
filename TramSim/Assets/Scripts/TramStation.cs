14d4945429a88082fa8284e57585d13c8510e723
GoodDeveloper-coder <itmancik@gmail.com>
2026-05-22 23:54:44 +0300

    Added tram station

---
+++ TramSim/Assets/Scripts/TramStation.cs
@@ -0,0 +1,62 @@
+using TMPro;
+using UnityEngine;
+
+public class TramStation : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float _minDistance = 30f;
+    [SerializeField] private float _waitTime = 10f;
+    [SerializeField] private Transform _tram;
+
+    [Header("UI")]
+    [SerializeField] private TMP_Text _waitTimeText;
+    private float _currentWaitTime = 0f;
+    private bool _nextStation = false;
+    private bool _isWaiting = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(_tram.position, transform.position) <= _minDistance)
+        {
+            if (!_isWaiting)
+            {
+                _isWaiting = true;
+                _waitTimeText.gameObject.SetActive(true);
+            }
+
+            UpdateWaitTime();
+        }
+        else
+        {
+            if (_isWaiting)
+                _waitTimeText.gameObject.SetActive(false);
+
+            _isWaiting = false;
+            _currentWaitTime = 0f;
+        }
+    }
+
+    void UpdateWaitTime()
+    {
+        if (_nextStation)
+            return;
+
+        if (_currentWaitTime >= _waitTime)
+        {
+            _nextStation = true;
+            _isWaiting = false;
+            _currentWaitTime = 0f;
+            _waitTimeText.gameObject.SetActive(false);
+        }
+        else
+        {
+            _currentWaitTime += Time.deltaTime;
+            _waitTimeText.text = string.Format("{0:#.0}", _currentWaitTime);
+        }
+    }
+}
